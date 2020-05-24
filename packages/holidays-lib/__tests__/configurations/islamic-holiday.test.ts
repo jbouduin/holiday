d0fb45161328b68fb448552db1b58c02f388e737
Johan Bouduin <johan@der-e-coach.de>
2020-05-24 10:14:35 +0200

    #961: Refactor tests: IslamicHoliday

--- packages/holidays-lib/__tests__/configurations/islamic-holiday.test.ts
+++ packages/holidays-lib/__tests__/configurations/islamic-holiday.test.ts
@@ -1,37 +1,39 @@
 import * as path from 'path';
 
-import { Configuration } from '../../src/configuration/configuration';
-import { IslamicHolidayType } from '../../src/configuration/holidays/islamic-holiday-type';
-import { IslamicHoliday } from '../../src/configuration/holidays/islamic-holiday';
-import { CycleType } from '../../src/configuration/holidays/cycle-type';
-import { BaseHoliday } from '../../src/configuration/holidays/base-holiday';
-import { HolidayStatus } from '../../src/configuration/holidays/holiday-status';
-import { HolidayType } from '../../src/configuration/holidays/holiday-type';
+import { Configuration } from '../../src/configuration';
+import { IIslamicHoliday, IslamicHolidayType } from '../../src/configuration';
 
-const dataRoot = './data';
-const type = 'islamic-holiday';
+const dataRoot = './data/islamic-holiday';
 
-describe(`${type}-only-type`, () => {
-  const current = 'only-type';
-  const fileName = path.join(__dirname, `${dataRoot}/${type}/${current}.json`);
-  const configuration = Configuration.loadByFileName(fileName);
-  test('hierarchy', () => expect(configuration.hierarchy).toBe(`${type}-${current}`));
-  test('description', () => expect(configuration.description).toBe(`${type}-${current}`));
-  test('collection size', () => expect(configuration.holidayCollection.length).toBe(1));
-  const islamicHoliday: IslamicHoliday = configuration.holidayCollection[0] as IslamicHoliday;
-  test('holidayType', () => expect(islamicHoliday.holidayType).toBe(HolidayType.ISLAMIC));
-  test('cycleType', () => expect(islamicHoliday.cycleType).toBe(CycleType.EVERY_YEAR));
-  test('holidayStatus', () => expect(islamicHoliday.holidayStatus).toBe(HolidayStatus.OFFICIAL_HOLIDAY));
-  test('validFrom', () => expect(islamicHoliday.validFrom).toBe(BaseHoliday.undefinedValidFrom));
-  test('validTo', () => expect(islamicHoliday.validTo).toBe(BaseHoliday.undefinedValidTo));
-  test('key', () => expect(islamicHoliday.key).toBe(IslamicHolidayType.ASCHURA));
-  test('translationKey', () => expect(islamicHoliday.translationKey).toBe('ASCHURA'));
-});
-
-describe(`Invalid Islamitic Configurations`, () => {
-  const fileName = path.join(__dirname, `${dataRoot}/${type}/invalid.json`);
-  const configuration = Configuration.loadByFileName(fileName);
-  test('collection size', () => expect(configuration.holidayCollection.length).toBe(3));
+describe.each([
+  ['holiday-type.aschura', IslamicHolidayType.ASCHURA],
+  ['holiday-type.id_al_fitr', IslamicHolidayType.ID_AL_FITR],
+  ['holiday-type.id_ul_adha', IslamicHolidayType.ID_UL_ADHA],
+  ['holiday-type.lailat_al_barat', IslamicHolidayType.LAILAT_AL_BARAT],
+  ['holiday-type.lailat_al_miraj', IslamicHolidayType.LAILAT_AL_MIRAJ],
+  ['holiday-type.lailat_al_qadr', IslamicHolidayType.LAILAT_AL_QADR],
+  ['holiday-type.mawlid_an_nabi', IslamicHolidayType.MAWLID_AN_NABI],
+  ['holiday-type.newyear', IslamicHolidayType.NEWYEAR],
+  ['holiday-type.ramadan', IslamicHolidayType.RAMADAN]
+])('Islamic holiday', (fileName: string, expected: IslamicHolidayType) => {
+  const file = path.join(__dirname, `${dataRoot}/${fileName}.json`);
+  const configuration = Configuration.loadByFileName(file);
+  test(`${fileName} - collection length`, () => expect(configuration.holidayCollection.length).toBe(1));
   const validation = configuration.validate();
-  test('number of errors', () => expect(validation.length).toBe(3));
-});
+  test(`${fileName} - validation length`, () => expect(validation.length).toBe(0));
+  const holiday: IIslamicHoliday = configuration.holidayCollection[0] as IIslamicHoliday;
+  test(`${fileName} - EthiopianOrthodoxHolidayType`,
+     () => expect(IslamicHolidayType[holiday.key]).toBe(IslamicHolidayType[expected]));
+})
+
+describe.each([
+  ['invalid.empty-holiday-type'],
+  ['invalid.holiday-type-value'],
+  ['invalid.missing-holiday-type']
+])('Islamic holiday invalid configurations', (fileName: string) => {
+  const file = path.join(__dirname, `${dataRoot}/${fileName}.json`);
+  const configuration = Configuration.loadByFileName(file);
+  test(`${fileName} - collection length`, () => expect(configuration.holidayCollection.length).toBe(1));
+  const validation = configuration.validate();
+  test(`${fileName} - validation length`, () => expect(validation.length).toBe(1));
+})
