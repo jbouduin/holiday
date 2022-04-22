ae811b6057e210067e84d4069696d94e1cf7691b
Johan Bouduin <johan@der-e-coach.de>
2022-04-22 16:56:04 +0200

    2087 adapt holidays test

--- packages/holidays-http/__tests__/file-provider.test.ts
+++ packages/holidays-http/__tests__/file-provider.test.ts
@@ -4,6 +4,7 @@ import { FileProvider } from '../src/file-provider';
 let host: string;
 let assetsPath: string;
 let scope: nock.Scope;
+const answer = { found: true};
 
 //#region setup/teardown ------------------------------------------------------
 beforeAll(() => {
@@ -18,81 +19,81 @@ afterAll(() => nock.restore());
 
 test('load-hierarchies', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/configurations.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/configurations.json`).reply(200, answer)
   return provider.loadHierarchies().then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer));
     scope.done();
   });
 });
 
 test('load-languages', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/languages.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/languages.json`).reply(200, answer)
   return provider.loadLanguages().then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer))
     scope.done();
   });
 });
 
 test('load-configurations', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/configurations/de.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/configurations/de.json`).reply(200, answer)
   return provider.loadConfiguration('de').then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer))
     scope.done();
   });
 });
 
 test('load-hierachy-translations > en', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/translations/hierarchy.de.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/translations/hierarchy.de.json`).reply(200, answer)
   return provider.loadHierarchyTranslations('de').then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer))
     scope.done();
   });
 });
 
 test('load-hierachy-translations > empty', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/translations/hierarchy.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/translations/hierarchy.json`).reply(200, answer)
   return provider.loadHierarchyTranslations('').then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer))
     scope.done();
   });
 });
 
 test('load-hierachy-translations > undefined', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/translations/hierarchy.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/translations/hierarchy.json`).reply(200, answer)
   return provider.loadHierarchyTranslations().then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer))
     scope.done();
   });
 });
 
 test('load-holidays-translations > en', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/translations/holiday.en.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/translations/holiday.en.json`).reply(200, answer)
   return provider.loadHolidayTranslations('en').then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer))
     scope.done();
   });
 });
 
 test('load-holidays-translations > empty', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/translations/holiday.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/translations/holiday.json`).reply(200, answer)
   return provider.loadHolidayTranslations('').then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer))
     scope.done();
   });
 });
 
 test('load-holidays-translations > undefined', () => {
   const provider = new FileProvider(host, assetsPath);
-  scope.get(`/${assetsPath}/translations/holiday.json`).reply(200, 'found')
+  scope.get(`/${assetsPath}/translations/holiday.json`).reply(200, answer)
   return provider.loadHolidayTranslations().then((result: string) => {
-    expect(result).toBe<string>('found');
+    expect(result).toBe<string>(JSON.stringify(answer))
     scope.done();
   });
 });
--- packages/holidays-http/src/file-provider.ts
+++ packages/holidays-http/src/file-provider.ts
@@ -23,31 +23,41 @@ export class FileProvider implements IFileProvider {
   //#region IFileProvider methods ---------------------------------------------
   public loadConfiguration(rootHierarchy: string): Promise<string> {
     const fileName = `${this.assetsPath}/configurations/${rootHierarchy}.json`;
-    return this.axios.get(fileName).then((response: AxiosResponse<string, any>) => response.data);
+    return this.axios
+      .get(fileName)
+      .then((response: AxiosResponse<string, any>) => JSON.stringify(response.data));
   }
 
   public loadHierarchies(): Promise<string> {
     const fileName = `${this.assetsPath}/configurations.json`;
-    return this.axios.get(fileName).then((response: AxiosResponse<string, any>) => response.data);
+    return this.axios
+      .get(fileName)
+      .then((response: AxiosResponse<string, any>) => JSON.stringify(response.data));
   }
 
   public loadHierarchyTranslations(language?: string): Promise<string> {
     const fileName = language ?
       `${this.assetsPath}/translations/hierarchy.${language}.json` :
       `${this.assetsPath}/translations/hierarchy.json`;
-    return this.axios.get(fileName).then((response: AxiosResponse<string, any>) => response.data);
+    return this.axios
+      .get(fileName)
+      .then((response: AxiosResponse<string, any>) => JSON.stringify(response.data));
   }
 
   public loadHolidayTranslations(language?: string): Promise<string> {
     const fileName = language ?
       `${this.assetsPath}/translations/holiday.${language}.json` :
       `${this.assetsPath}/translations/holiday.json`;
-    return this.axios.get(fileName).then((response: AxiosResponse<string, any>) => response.data);
+    return this.axios
+      .get(fileName)
+      .then((response: AxiosResponse<string, any>) => JSON.stringify(response.data));
   }
 
   public loadLanguages(): Promise<string> {
     const fileName = `${this.assetsPath}/languages.json`;
-    return this.axios.get(fileName).then((response: AxiosResponse<string, any>) => response.data);
+    return this.axios
+      .get(fileName)
+      .then((response: AxiosResponse<string, any>) => JSON.stringify(response.data));
   }
   //#endregion
 }
