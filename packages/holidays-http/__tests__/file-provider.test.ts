fb154a92921ef8ee43512f5316f9969739a4734f
Johan Bouduin <johan@der-e-coach.de>
2022-04-22 12:21:16 +0200

    2087 adapt holidays test

--- packages/holidays-http/__tests__/file-provider.test.ts
+++ packages/holidays-http/__tests__/file-provider.test.ts
@@ -20,7 +20,6 @@ test('load-hierarchies', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/configurations.json`).reply(200, 'found')
   return provider.loadHierarchies().then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
@@ -30,42 +29,24 @@ test('load-languages', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/languages.json`).reply(200, 'found')
   return provider.loadLanguages().then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
 });
-
-// test('load-hierarchies > file not found', async () => {
-//   const provider = new FileProvider(host, assetsPath);
-//   return provider.loadHierarchies().catch(e => expect(e.code).toMatch('ENOENT'));
-// });
 
 test('load-configurations', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/configurations/de.json`).reply(200, 'found')
   return provider.loadConfiguration('de').then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
 });
 
-// test.only('load-configurations > file not found > non existing hierarchy', async () => {
-//   const provider = new FileProvider(host, assetsPath);
-//   scope.get(`/${assetsPath}/configurations/12.json`).reply(404)
-//   return provider.loadConfiguration('12').then((result: string) => {
-//     console.log(result);
-
-//     scope.done();
-//   });
-// });
-
 test('load-hierachy-translations > en', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/translations/hierarchy.de.json`).reply(200, 'found')
   return provider.loadHierarchyTranslations('de').then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
@@ -75,7 +56,6 @@ test('load-hierachy-translations > empty', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/translations/hierarchy.json`).reply(200, 'found')
   return provider.loadHierarchyTranslations('').then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
@@ -85,7 +65,6 @@ test('load-hierachy-translations > undefined', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/translations/hierarchy.json`).reply(200, 'found')
   return provider.loadHierarchyTranslations().then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
@@ -95,7 +74,6 @@ test('load-holidays-translations > en', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/translations/holiday.en.json`).reply(200, 'found')
   return provider.loadHolidayTranslations('en').then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
@@ -105,7 +83,6 @@ test('load-holidays-translations > empty', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/translations/holiday.json`).reply(200, 'found')
   return provider.loadHolidayTranslations('').then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
@@ -115,7 +92,6 @@ test('load-holidays-translations > undefined', () => {
   const provider = new FileProvider(host, assetsPath);
   scope.get(`/${assetsPath}/translations/holiday.json`).reply(200, 'found')
   return provider.loadHolidayTranslations().then((result: string) => {
-    console.log(result);
     expect(result).toBe<string>('found');
     scope.done();
   });
